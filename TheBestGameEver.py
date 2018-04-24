7a2069a5a7c2492b51a60f714f1db6c31d58e6b4
Hansel Leal <Hansel517@Outlook.com>
2018-04-24 15:59:48 -0400

    added outline in comments for snake reboot

--- TheBestGameEver.py
+++ TheBestGameEver.py
@@ -1,3 +1,36 @@
 #this is where most or all of the code for the game will be contained
 #We'll start by commenting the game and then fill in the rest with code later.
 print("Hello World!")
+
+#open a game window
+    #show main menu screen
+        #option to play
+        #option for how to play
+        #option high scores
+        #game options
+            #sound
+            #game speed
+            #start-length
+
+        #create game objects
+            #movable area
+            #score
+            #snake
+            #apples - of different types
+        #set stage for game
+            #place snake
+            #place the apple
+    #main game loop begins
+        #move snake
+        #check for user input and adjust direction accordingly
+        #when you eat an apple
+            #increment score
+            #add length to snake body
+            #place new apple
+        #check for collision with wall or self
+            #if a collision occurs
+                #end main game loop
+    #show game over screen
+    #ask user for name to save high score
+    #display top scores
+    #ask user to play again
