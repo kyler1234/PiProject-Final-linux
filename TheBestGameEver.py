5dcdd78393618b3a470843506006929caa9f6cb2
Unknown <Maverickfox@outlook.com>
2018-05-09 22:11:28 -0400

    Added a Snake class
    After some dabbling, grid based movement is kind of the only way to go unless we want to spend time doing free form slithering controls.  Since we don't have that kind of time I made a basic snake class which relies on grid based movement along with some constants to help down the line. Don't know if it works as i haven't gotten to re-doing the main game loop but time will tell.

--- TheBestGameEver.py
+++ TheBestGameEver.py
@@ -33,6 +33,98 @@
     #display top scores
     #ask user to play again
 
+#directional constants
+DIRECTION_UP = 0
+DIRECTION_DOWN = 1
+DIRECTION_LEFT = 2
+DIRECTION_RIGHT = 3
+
+#look up table for keys and directions
+KEY_DIRECTION = {
+    K_w: DIRECTION_UP,    K_UP:    DIRECTION_UP,   
+    K_s: DIRECTION_DOWN,  K_DOWN:  DIRECTION_DOWN, 
+    K_a: DIRECTION_LEFT,  K_LEFT:  DIRECTION_LEFT, 
+K_d: DIRECTION_RIGHT, K_RIGHT: DIRECTION_RIGHT,
+}
+
+#color constants
+SNAKE_HEAD_COLOR = (250, 50, 50)
+SNAKE_BODY_COLOR = (250, 250, 250)
+APPLE_COLOR = (50, 250, 50)
+
+#grid size constant
+GRID = (50, 50) # X, Y
+
+#snake based constants
+SNAKE_SPEED_INITIAL = 4.0       # Initial snake speed
+SNAKE_SPEED_INCREMENT = 0.25    # Snake speeds up this much each time it grows
+SNAKE_START_LENGTH = 4          # Initial snake length in segments
+SNAKE_START_LOC = (250, 250)    # Initial snake location
+
+#snake class
+#init with a starting point as a tupple and length in number of grid squares
+class Snake:
+    def __init__ (self, start, length):
+        self.startLength = startLength
+        self.speed = SNAKE_SPEED_INITIAL
+        self.start = start
+        self.reset()
+
+    #reset snake to original state
+    def reset(self):
+        self.snakeBits = []
+        self.direction = DIRECTION_UP
+
+        for n in range(0, self.startLength):
+            self.snakeBits.append((self.start[0], self.start[1] + n))
+
+    #change directions if not in opposite direciton
+    def change_direction(self, direction):
+        if not (self.direction == 0 and direction == 1 or
+            self.direction == 1 and direction == 0 or
+            self.direction == 2 and direction == 3 or
+            self.direction == 3 and direction == 2):
+        self.direction = direction
+
+    # Returns the head of the snake
+    def getHead(self):
+        return self.snakeBits[0]
+
+    # Returns the tail of the snake
+    def getTail(self):
+        return self.snakeBits[len(self.snakeBits) - 1]
+
+    def move_snake(self):
+        head = self.getHead();
+        newHead = ()
+
+        if self.direction = 0: newHead = (head[0], head[1] + 1)
+        if self.direction = 1: newHead = (head[0], head[1] - 1)
+        if self.direction = 2: newHead = (head[0] + 1, head[1])
+        if self.direction = 3: newHead = (head[0] - 1, head[1])
+
+        self.snakeBits.pop()
+        self.snakeBits.insert(0, newHead)
+
+    def add_bit(self):
+        tail = self.getTail();
+        bit = ()
+
+        if self.direction = 0: bit = (tail[0], tail[1] + 1)
+        if self.direction = 1: bit = (tail[0], tail[1] - 1)
+        if self.direction = 2: bit = (tail[0] + 1, tail[1])
+        if self.direction = 3: bit = (tail[0] - 1, tail[1])
+
+        self.snakeBits.append(bit)
+
+    #check if the head is located in the same cell as another bit of the snake
+    def check_collision(self):
+        if len([bit for bit in self.snakeBits if bit == self.getHead()]) > 1:
+            return True
+        else:
+            return False
+
+
 print("Hello World!")
 import sys, pygame
 
